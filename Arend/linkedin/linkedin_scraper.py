9fd1b0f25a2f8229d4180eb31e396f75c8f3227e
jpkunkler <pascal.kunkler@gmail.com>
2017-01-02 15:30:47 +0100

    Initialized GitHub repo

---
+++ Arend/linkedin/linkedin_scraper.py
@@ -0,0 +1,87 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import csv
+import json
+import time
+import re
+
+driver = webdriver.Firefox()
+login = "https://www.linkedin.com/uas/login"
+
+pages= ["https://www.linkedin.com/company-beta/8545501/",
+        "https://www.linkedin.com/company-beta/1062181/",
+        "https://www.linkedin.com/company-beta/3850436",
+        "https://www.linkedin.com/company-beta/9357034/",
+        "https://www.linkedin.com/company-beta/62059/"
+        ]
+
+count = 0
+
+# Login as User
+driver.get(login)
+email = driver.find_element_by_id("session_key-login")
+password = driver.find_element_by_id("session_password-login")
+email.send_keys("pascal.kunkler@gmail.com")
+password.send_keys("Pascalk_170395")
+driver.find_element_by_name("signin").click()
+time.sleep(5)
+
+with open('linkedin_analysis.csv', 'w') as file:
+    w = csv.writer(file)
+    w.writerow(['company', 'employees', 'employees_on_linkedin', 'follower',
+                'updates', 'open_jobs'])
+
+    #Scrape pages
+    for profile in pages:
+        driver.get(profile)
+        time.sleep(5)
+
+        #Scroll to bottom and try as long as change appears
+        lastHeight = driver.execute_script("return document.body.scrollHeight")
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+            newHeight = driver.execute_script("return document.body.scrollHeight")
+            if newHeight == lastHeight:
+                break
+            lastHeight = newHeight
+
+        html=driver.page_source
+        soup=BeautifulSoup(html, "lxml")
+
+        name = soup.find('h1', {'class':'company-main-info-company-name'}).getText().replace('employees', '').replace('\n', '')
+        size = soup.find('span', {'class':'company-size'}).getText().replace(' ', '').replace(',', '')
+        size_d = re.sub(r'[a-z]', "", size).replace('\n', '')
+        employees = soup.find_all('a', {'class':'snackbar-description-see-all-link'})
+        employees_count = re.sub("\D", "", employees[0].getText())
+        updates = len(soup.find_all('article'))
+        follower = soup.find_all('p', {'class':'top-card-followers-count Sans-15px-black-55%'})
+        follower_count = re.sub("\D", "", follower[0].getText())
+
+        driver.execute_script("window.scrollTo(0, 0);")
+        time.sleep(2)
+        print "Now trying to go to Jobs page..."
+        link = driver.find_element_by_link_text('See jobs')
+        link.click()
+        time.sleep(5)
+
+        html1 = driver.page_source
+        soup1 = BeautifulSoup(html1, "lxml")
+        jobs = soup1.find('div', {'class':'results-context'})
+        jobs_count = re.sub("\D", "", jobs.getText())
+
+        print "-"*20
+        print "Company: ", name
+        print "Size: ", size_d
+        print "Follower: ", follower_count
+        print "Employees on LinkedIn: ", employees_count
+        print "Updates: ", updates
+        print "Current Jobs: ", jobs_count
+        print "-"*20
+
+        w.writerow([name, size_d, employees_count, follower_count,
+                    updates, jobs_count])
+        count += 1
+
+print "\nSuccess! {} companies scraped.".format(count)
+driver.close()
---
+++ Arend/xing/xing_scraper.py
@@ -0,0 +1,85 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import csv
+import json
+import time
+import re
+
+driver = webdriver.Firefox()
+
+pages= ["https://www.xing.com/companies/blumenbeckergruppe",
+        "https://www.xing.com/companies/arendprozessautomationgmbh",
+        "https://www.xing.com/companies/ksvkoblenzersteuerungs-undverteilungsbaugmbh",
+        "https://www.xing.com/companies/beckhoffautomationgmbh&co.kg"
+        ]
+with open('xing_analysis.csv', 'w') as file:
+    w = csv.writer(file)
+    w.writerow(['company', 'employees', 'employees_on_xing', 'rating', 'awards',
+                'updates', 'open_jobs'])
+
+    for profile in pages:
+        driver.get(profile)
+        html1=driver.page_source
+        soup1=BeautifulSoup(html1, "lxml")
+
+        #About Page (MAIN PAGE)
+        name = soup1.find('div', {"class":"header-name"}).getText().replace('\n', '')
+        facts=soup1.find('section', {"class": "facts"})
+        employees = facts.find('dd').getText()
+        try:
+            rating = soup1.find('div', {"class":"kununu-rating-nr"}).getText().replace('\n', '').replace(' ', '')
+            awards = soup1.find_all('a', {"class":"kununu-badge"})
+        except:
+            rating = None
+            awards = []
+
+        #Updates Page
+        try:
+            link = driver.find_element_by_link_text('Updates')
+            link.click()
+        except:
+            posts = []
+
+        while True:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            try:
+                button = driver.find_element_by_link_text('More')
+                if button:
+                    if button.is_displayed():
+                        button.click()
+                else:
+                    break
+            except:
+                break
+
+        html2=driver.page_source
+        soup2=BeautifulSoup(html2, "lxml")
+
+        #Get updates
+        updates = []
+        for ul in soup2.find_all('ul', {'id': 'news-feed'}):
+            for li in ul.find_all('li', {'class':'activity-item'}):
+                updates.append(li)
+
+        #Get employees
+        for li in soup2.find_all('li', {'id':'employees-tab'}):
+            for a in li.find_all('a'):
+                contacts = re.sub("\D", "", a.getText())
+
+        #Get jobs
+        for li in soup2.find_all('li', {'id':'jobs-tab'}):
+            for a in li.find_all('a'):
+                jobs = re.sub("\D", "", a.getText())
+
+        print json.dumps({"name": name, "data": {
+                                "employees": employees,
+                                "rating": rating,
+                                "updates": len(updates),
+                                "contacts": contacts,
+                                "awards": len(awards),
+                                "jobs": jobs
+                                }})
+
+        w.writerow([name, employees, contacts, rating, len(awards), len(updates), jobs])
+
+driver.close()
